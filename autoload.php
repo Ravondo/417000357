f6ae6694fb5b9eaa144311c61a86de6ac5f75420
Ravondo <36017586+Ravondo@users.noreply.github.com>
2020-12-06 17:16:37 -0400

    Add files via upload

--- autoload.php
+++ autoload.php
@@ -1,22 +1,21 @@
 <?php
-spl_autoload_register(function($class) {
-	if (!defined('APP_DIR')) {
-		define ("ROOT_DIR", 'C:\xampp_2\htdocs\assignment_1');
-		define ("APP_DIR", ROOT_DIR . "\app");
-		define ("FRAMEWORK_DIR", ROOT_DIR . '\framework');
-		define ('TPL_DIR', ROOT_DIR . '\tpl');
-		define ('DATA_DIR', ROOT_DIR . '\data');
-	}
-	if (file_exists(FRAMEWORK_DIR . "/" . $class . '.php')) {
-		require FRAMEWORK_DIR . '/' . $class . '.php';
-	}
-	else if (file_exists(APP_DIR . "/" . $class . '.php')) {
+spl_autoload_register(function($classname) {
+	$parts = explode('\\', $classname);
+	$class = $parts[count($parts) - 1];
+
+	if (file_exists(APP_DIR . "/" . $class . '.php')) {
 		require APP_DIR . '/' . $class . '.php';
+	}
+	else if (file_exists(FRAMEWORK_DIR . "/" . $class . '.php')) {
+		require FRAMEWORK_DIR . '/' . $class . '.php';
 	}
 	else if (file_exists(TPL_DIR . "/" . $class . '.php')) {
 		require TPL_DIR . '/' . $class . '.php';
 	}
 	else if (file_exists(DATA_DIR . "/" . $class . '.php')) {
 		require DATA_DIR . '/' . $class . '.php';
+	}
+	else {
+		trigger_error('Cannot find class/interface/abstract definition: '. $class, E_USER_ERROR);
 	}
 });
---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define("ROOT_DIR", 'C:\xampp2\htdocs\assignment_1');
+define("APP_DIR", ROOT_DIR . "\app");
+define("FRAMEWORK_DIR", ROOT_DIR . '\framework');
+define('TPL_DIR', ROOT_DIR . '\tpl');
+define('DATA_DIR', ROOT_DIR . '\data');
