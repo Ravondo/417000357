4dae817264978a18209717d9cf44d71b6488e718
Ravondo <36017586+Ravondo@users.noreply.github.com>
2020-12-06 16:53:33 -0400

    Update FrontController.php

--- FrontController.php
+++ FrontController.php
@@ -3,12 +3,12 @@ class FrontController extends FrontController_Abstract
 {
     private function __construct()
     {
-
+        $this->$req = Registry::instance();
     }
 
     private function __clone()
     {
-
+        $this->$controller = clone $this->$controller;
     }
 
     public static function run()
@@ -20,18 +20,23 @@ class FrontController extends FrontController_Abstract
 
     protected function init()
     {
-        
+        $this->$req->get('application helper')->init();
     }
 
     protected function handleRequest()
     {
-        $context = new CommandContext();
+        /*$context = new CommandContext();
         //$req = (string) $context->get('request');
         $handler = RequestHandlerFactory::makeRequestHandler();
         
         if($handler->execute($context) === false)
         {
             //do some error handling
-        }
+        }*/
+
+        $request = $req->getRequest();
+        $context = new CommandContext();
+        $cmd = $context->getCommand($request);
+        $cmd->execute($request);
     }
 }
