3c01503c99df42cd61445ad6d162ef79a683ac58
Ravondo <36017586+Ravondo@users.noreply.github.com>
2020-12-06 16:49:45 -0400

    Update CommandContext.php

--- CommandContext.php
+++ CommandContext.php
@@ -3,14 +3,21 @@ namespace Quwi\framework;
 
 class CommandContext extends CommandContext_Abstract
 {
+    protected $data = [];
+    protected $errors = [];
+
     public function add(string $key, $val)
     {
-
+        $this->data = $_REQUEST;
     }
 
     public function get(string $key)
     {
-
+        if(isset($this->data[$key]))
+        {
+            return $this->data[$key];
+        }
+        return null;
     }
 
     public function getErrors(): array
@@ -20,6 +27,6 @@ class CommandContext extends CommandContext_Abstract
 
     protected function setError($error)
     {
-         
+         $this->error = $error;
     }
 }
