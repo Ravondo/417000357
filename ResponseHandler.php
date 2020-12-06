81db22974e0fcb8a2536b9e070b8ad166f88abce
Ravondo <36017586+Ravondo@users.noreply.github.com>
2020-12-06 17:14:31 -0400

    Update ResponseHandler.php

--- ResponseHandler.php
+++ ResponseHandler.php
@@ -1,27 +1,27 @@
 <?php
 class ResponseHandler extends ResponseHandler_Interface
 {
-    protected body = [];
+    protected $data = [];
 
-    public function __construct(ResponseHeader $head, ResponseState $state, ResponseLogger $log)
+    public function __construct(ResponseHeader $head, State $state, Logger $log)
     {
-        $this->body['header'] = $head;
-        $this->body['state'] = $state;
-        $this->body['log'] = $log;
+        $this->data['header'] = $head;
+        $this->data['state'] = $state;
+        $this->data['log'] = $log;
     }
 
     public function giveHeader() : ResponseHeader
     {
-        return clone $this->body['header'];
+        return clone $this->data['header'];
     }
 
     public function giveState() : ResponseState
     {
-        return clone $this->body['state'];
+        return clone $this->data['state'];
     }
 
     public function giveLogger() : ResponseLogger
     {
-        return clone $this->body['log'];
+        return clone $this->data['log'];
     }
 }
